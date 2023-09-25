1e161d876f9260089adc226e9c18e662cead6dd5
jr <141987564+jrguedes7@users.noreply.github.com>
2023-09-24 22:42:19 -0300

    first commit

---
+++ get/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <form action="processamento.php" method="get">
+   <div>
+    <input type="text" name="nome" placeholder="preencha seu nome"><br><br>
+   </div>
+   <div>
+     <input type="submit" value="enviar">
+   </div>
+  </form>  
+</body>
+</html>
---
+++ get/processamento.php
@@ -0,0 +1,4 @@
+<?php
+$nome = $_GET['nome'];
+?>
+<h1>O seu nome é <?= $nome ?></h1>
